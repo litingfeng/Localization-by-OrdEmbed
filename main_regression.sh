0b69a6205825275eb8f2e5833c4d6630bc928a4c
AmberLi <tl601@cs.rutgers.edu>
2021-03-28 14:47:15 -0400

    remove all files in main

--- main_regression.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python ordinal-regression.py \
-                       --savename order_reg_cumloss \
-                       --lr 0.001 \
-                       --angle_step 15 \
-                       --mode cum_loss \
-                       --optimizer Adam \
-                       --epochs 200 \
-                       --batch_size 512 \
-                       --step 80
